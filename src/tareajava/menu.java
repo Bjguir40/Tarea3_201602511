ba0a13a555948a453493b842223b68310b22d70c
Byron G <Byron G@Byron>
2017-08-09 16:02:48 -0600

    Tarea 3 finalizada con Éxito

--- src/tareajava/menu.java
+++ src/tareajava/menu.java
@@ -25,16 +25,20 @@ public class menu {
     public void menu1() {
 
         while (flag) {
-
+    
             System.out.println("      TAREA 3");
             System.out.println("     201602511");
             System.out.println("1. Usuarios");
             System.out.println("2. Palabras Palindromas");
             System.out.println("3. Salir");
+            try{
             Scanner sc = new Scanner(System.in);
             opcion = sc.nextInt();
-            
-
+         
+            }catch(Exception e){
+                System.out.println("Inserte una opcon valida");
+                menu1();
+            }
             switch (opcion) {
 
                 case 1:
@@ -44,8 +48,14 @@ public class menu {
                     System.out.println("3. Mostrar Un Usuraio Personalizado");
                     System.out.println("4. Menu Principal");
                     System.out.println("5. Salir");
+                    try{
                     Scanner sc1 = new Scanner(System.in);
                     opcion1 = sc1.nextInt();
+                    }catch(Exception e){
+                        System.out.println("Inserte una opción valida");
+                        
+                    }
+                            
                     switch (opcion1) {
 
                         case 1:
@@ -72,20 +82,18 @@ public class menu {
                             break;
                 
                     }
-
                     break;
 
                 case 2:
                     
                     System.out.println("PALABRAS PALINDROMAS");
                     System.out.println("Ingresar Palabra");
-                    
+                    menupalindromas();
                     break;
 
                 case 3:
                     flag = false;
-                    break;
-                
+                    break;      
 
             }
         }
@@ -96,25 +104,60 @@ public class menu {
     public void upersonalizado() {
 
         System.out.println("Mostrar un Usuario Personalizado");
+        System.out.println("Ingresar Usuario");
         boolean encontrado = false;
         Scanner S = new Scanner(System.in);
         String nombreingresado = S.nextLine();
         int z = 0;
+        System.out.println("Usuario");
         while (z < usuario.length && !encontrado) {
             if (nombreingresado.equals(usuario[z])) {
                 System.out.println(usuario[z]);
                 encontrado = true;
+
             }
             z++;
         }
+
         if (!encontrado) {
             System.out.println("Usuario no encontrado");
         }
     }
-    
+    public void menupalindromas(){
+     Scanner sc =new Scanner(System.in);
+     
+        String frase = sc.nextLine();
+        if(esPalindroma(frase)){
+            System.out.println("La Palabra si es palindroma");
+        
+        }else{
+            System.out.println("La Palabra no es palindroma");
+        }          
+  }      
+      public static boolean esPalindroma(String frase){   
+      String aux="";
+        int n = frase.length();
+        for(int i = 0; i < n; i++){
+           if(!(frase.substring(i, i + 1).equals(" ")
+                   || frase.substring(i, i + 1).equals(",")
+                   || frase.substring(i, i + 1).equals("¿")
+                   || frase.substring(i, i + 1).equals("?")
+                   || frase.substring(i, i + 1).equals(".")
+                   || frase.substring(i, i+ 1).equals("!")
+                   || frase.substring(i, i + 1).equals("¡"))){
+        
+               aux = aux + frase.substring(i, i + 1);
+               }
+        }
+        n = aux.length();
+        for (int i = 0; i < n /2; i++){
+         if(!aux.substring(i, i + 1).equals(aux.substring(n - i - 1, n - i))){
+         return false;       
          
          }
-    
+    }
+      return true;
       
-                    
+}                    
+}                   
 
