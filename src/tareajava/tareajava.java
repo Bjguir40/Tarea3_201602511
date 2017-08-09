8b87f56fc09b0544eb024ae2d43ade32dec4f389
Byron G <Byron G@Byron>
2017-08-09 16:56:11 -0600

    Tarea 3 terminada con exito 2

---
+++ src/tareajava/tareajava.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Tareajava;
+
+/**
+ *
+ * @author Byron G
+ */
+public class tareajava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        menu principal =new menu();
+        principal.menu1();
+        
+    }
+    
+}
